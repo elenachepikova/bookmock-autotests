09ecd8556b90edc55eb75586d3a7158367b7de6b
Elena Chepikova <elena.v.chepikova@gmail.com>
2025-01-14 17:28:33 +0100

    add contact page tests

---
+++ tests/test_contact.py
@@ -0,0 +1,38 @@
+import allure
+
+from elements.footer import Footer
+from elements.header import NavigationPanel
+from pages.contact_page import ContactPage
+from pages.form_submitted_page import FormSubmittedPage
+from pages.home_page import HomePage
+
+
+@allure.suite("Tests for 'CONTACT' page")
+class TestContactPage:
+
+    @allure.title("Verify 'CONTACT' page is accessible via navigation panel")
+    def test_open_contact_page(self, driver):
+        homepage = HomePage(driver)
+        homepage.open()
+        panel = NavigationPanel(driver)
+        panel.click_on(panel.CONTACT)
+        contact_page = ContactPage(driver)
+        contact_page.assert_page_is_displayed()
+
+    @allure.title("Verify all expected elements are present on 'CONTACT' page")
+    def test_assert_contact_page_ui(self, driver):
+        contact_page = ContactPage(driver)
+        contact_page.open()
+        header = NavigationPanel(driver)
+        header.assert_header_is_displayed()
+        contact_page.assert_sections_are_present()
+        footer = Footer(driver)
+        footer.assert_footer_is_displayed()
+
+    @allure.title("'Contact Us' form can be successfully submitted if all fields are filled in")
+    def test_submit_contact_us_form(self, ff_driver):
+        contact_page = ContactPage(ff_driver)
+        contact_page.open()
+        contact_page.fill_in_contact_us_form()
+        form_submitted = FormSubmittedPage(ff_driver)
+        form_submitted.assert_page_is_displayed()
