5ded13c38873f2e6d71d0c46336fa81d3389b50f
Elena Chepikova <elena.v.chepikova@gmail.com>
2025-01-29 15:54:07 +0100

    added tests for cart

--- elements/cart.py
+++ elements/cart.py
@@ -19,7 +19,6 @@ class Cart(Assertions):
     CONTINUE_SHOPPING_BUTTON_E = (By.CSS_SELECTOR, 'button.btn.mt-4')
     CONTINUE_SHOPPING_BUTTON_F = (By.CSS_SELECTOR, 'button.bb-cart-continue-btn')
 
-
     def __init__(self, driver):
         super().__init__(driver)
 
@@ -33,8 +32,7 @@ class Cart(Assertions):
     def assert_empty_cart_is_displayed(self):
         self.assert_cart_sidebar_is_displayed()
         self.assert_element_is_visible(self.MESSAGE)
-        message = self.wait_for_element(self.MESSAGE)
-        assert message.text == 'Your Cart is Empty'
+        self.assert_text(self.MESSAGE, 'Your Cart is Empty')
         self.assert_element_is_visible(self.CONTINUE_SHOPPING_BUTTON_E)
 
     @allure.step('Assert Cart sidebar is not displayed')
@@ -55,8 +53,8 @@ class Cart(Assertions):
 
     @allure.step('Assert cart items count is {value}')
     def assert_cart_items_count(self, value):
-        assert self.count_elements(self.CART_ITEMS) == value, (f"Expected items count = {value}, "
-                                                             f"actual = {self.count_elements(self.CART_ITEMS)}")
+        assert self.count_elements(self.PRODUCT_NAME) == value, (f"Expected items count = {value}, "
+                                                              f"actual = {self.count_elements(self.PRODUCT_NAME)}")
 
     @allure.step('Assert cart item name is {title}')
     def assert_product_name(self, title):
@@ -66,18 +64,38 @@ class Cart(Assertions):
     def assert_product_price(self, value):
         self.assert_text(self.PRODUCT_PRICE, value)
 
-    @allure.step('Assert cart total')
-    def assert_cart_total(self, qty = None):
-        price = self.find_price_by_title()
-        if qty is None:
-            total = str(price)
-            self.assert_text(self.CART_TOTAL, total)
-        else:
-            total = str(price * qty)
-            self.assert_text(self.CART_TOTAL, total)
-
-    def find_price_by_title(self):
-        title = self.get_label(self.PRODUCT_NAME)
+    @staticmethod
+    def find_price_by_title(title):
         for product_key, product_info in products.items():
             if product_info["title"] == title:
                 return float(product_info["price"])
+
+    @allure.step('Update quantity value for product in cart')
+    def set_quantity(self, value: str):
+        self.select_dropdown_option(self.QUANTITY_SELECTOR, value)
+
+    @allure.step('Click on "Continue Shopping" button')
+    def click_on_continue_shopping_button(self):
+        self.click_on(self.CONTINUE_SHOPPING_BUTTON_F)
+
+    @allure.step('Assert total price of all items in cart')
+    def assert_cart_total(self):
+        items_count = self.count_elements(self.PRODUCT_NAME)
+        total = 0
+
+        product_titles = self.wait_for_elements(self.PRODUCT_NAME)
+
+        for i in range(items_count):
+            title = product_titles[i].text
+            qty = int(self.get_product_quantity(i))
+
+            price = self.find_price_by_title(title)
+            total += price * qty
+
+        self.assert_text(self.CART_TOTAL, str(total))
+
+    def get_product_quantity(self, index=0):
+        dynamic_selector = (By.NAME, f'sm-cart-item-qty-{index + 1}')
+        element = self.wait_for_element(dynamic_selector)
+        value = element.get_attribute("data-current")
+        return value
